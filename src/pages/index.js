ed95d8b3e9e178ffe3c3882e1edecc6acfe34029
preeme <roa.michael5@gmail.com>
2019-08-07 11:47:04 -0700

    changed home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,6 @@ const IndexPage = () => (
       <Head title="Home" />
       <h1>Hello</h1>
       <h2>I'm Michael, a full-stack developer living in Los Angeles</h2>
-      <p>
-        Need a developer? <Link to="/contact">Contact Me</Link>
-      </p>
     </Layout>
   </div>
 );
